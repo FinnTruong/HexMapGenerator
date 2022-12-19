42c6223e36a16abd79e82b02aebb9d47c5b5537a
Hiếu, Trương Minh (7) <tmhieu00@gmail.com>
2022-12-19 15:23:41 +0700

    feat: UI animation

---
+++ src/Assets/Scripts/SliderController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderController : MonoBehaviour
+{
+    public HexMapGenerator generator;
+
+    [Header("Island Slider")]
+    public Slider seedSlider;
+    public Slider refinementSlider;
+    public Slider tileSizeSlider;
+    public Slider tileHeightSlider;
+    public Slider radiusSlider;
+
+    //[Space]
+    //[Header("Ocean Slider")]
+
+    //[Space]
+    //[Header("Foliage Slider")]
+
+    //[Space]
+    //[Header("Terrain Slider")]
+}
+
